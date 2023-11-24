33ea36ca255410ad304fb0cedf3ee6e03b413c1f
Aaron Song <aaron@kmong.com>
2023-11-24 09:23:51 +0900

    feat: Custom 이미지 Composable 구현

--- app/src/main/java/song/aaron/composemodifierdemo/MainActivity.kt
+++ app/src/main/java/song/aaron/composemodifierdemo/MainActivity.kt
@@ -3,15 +3,25 @@ package song.aaron.composemodifierdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,11 +51,36 @@ fun DemoScreen() {
         .padding(all = 10.dp)
         .border(width = 2.dp, color = Color.Black)
 
-    Text(
-        text = "Hello Compose",
-        modifier = modifier,
-        fontSize = 40.sp,
-        fontWeight = FontWeight.Bold
+    Column(
+        modifier = Modifier.padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Hello Compose",
+            modifier = modifier,
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        CustomImage(image = R.drawable.vacation)
+    }
+
+}
+
+/**
+ * 커스텀 Composable
+ * - modifier 를 선택 인자로 받음
+ */
+@Composable
+fun CustomImage(image: Int, modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = image),
+        contentDescription = null,
+        modifier = modifier
+            .padding(16.dp)
+            .width(270.dp)
+            .clip(shape = RoundedCornerShape(30.dp))
     )
 }
 
